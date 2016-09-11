709d377c1a616a92b31e00a1ebc3c3eb9b90cf9c
fabricio.costa@moldeointeractive.com.ar <fabricio.costa@moldeointeractive.com.ar>
2016-09-11 14:37:29 -0300

    adding headers for all opencv features

--- Resources/OpenCV/moOpenCV.h
+++ Resources/OpenCV/moOpenCV.h
@@ -55,6 +55,7 @@
         #include "opencv2/objdetect/objdetect.hpp"
         //#include "opencv2/highgui/highgui.hpp"
         #include "opencv2/imgproc/imgproc.hpp"
+        #include <opencv2/opencv.hpp>
     #else
         #include "opencv2/features2d/features2d.hpp"
         #include "opencv2/objdetect/objdetect.hpp"
