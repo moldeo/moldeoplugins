f09f9e653752e0c786bc16535a6d00ab95a4eea8
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-11-01 20:21:32 -0300

    Fixing Regular Solid

--- IODevices/Ola/buildola.sh
+++ IODevices/Ola/buildola.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/OpenLightingProject/ola
 cd ola
 sudo apt-get install bison flex uuid-dev libcppunit-dev libprotoc-dev libftdi-dev liblo-dev libmicrohttpd-dev dh-autoreconf
+libtoolize
 automake --add-missing
 autoreconf
 ./configure --prefix=/usr
