be9eb0ac2b246cb2987105a592510a0236079850
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2017-11-27 14:43:13 -0300

    Adding configs

--- Resources/OpenFace/moOpenFace.cpp
+++ Resources/OpenFace/moOpenFace.cpp
@@ -1538,7 +1538,13 @@ moOpenFace::FaceDetection() {
   Mat frame_gray;
   cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
   //equalizeHist(frame_gray, frame_gray);
-
+  bool rotatem;
+  //M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+  //dst = cv2.warpAffine(img,M,(cols,rows))
+  cv::Mat M = cv::getRotationMatrix2D( cv::Point2f( m_reduce_width/2, m_reduce_height/2) , 90, 1 );
+  cv::Mat dst;
+  cv::warpAffine( frame_gray ,dst, M,cv::Point2f(m_reduce_width,m_reduce_height));
+  //cv::warpAffine(
 // The actual facial landmark detection / tracking
 
     cv::Mat_<float> depth_image;
